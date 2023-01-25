ee462daceabce05a54dedb6cbcaf735e96570ea8
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 15:13:48 +0200

    feat:moyenne

--- demo.php
+++ demo.php
@@ -1,50 +1,10 @@
-<?php echo 'Si vous voulez integrer du code PHP Dans le document XHTML ou XML, Utilisez ces balises'?>
-
-vous pouvez utiliser la balise courte pour  <?= 'écrire ce texte'?>.
-Ce dernier est équivalent à <?php echo 'écrire ce texte'?>.
-
-<? echo 'ce code est entre des balises courtes'; ?>
-
-le code suivant <?= 'du text' ?> est un raccourci pour <? echo 'du texte' ?>
-
-<p>
-    Note: Si un fichier contient seulement du code PHP , il est préférable de ne pas placer la balise de fermeture à la fin du fichier.
-    <br>
-    ceci permet d'éviter d'oublier un espace ou une nouvelle ligne après la balise de fermeture de PHP, ce qui causerait des effets non voulus car PHP,
-    Commencera à afficher la sortie, ce qui n'est souvent pas le cas désiré.
-</p>
-
 <?php
-echo "Bonjour le monde!";
-
-//... encore du codele 
-
-
-echo "Dernier instruction";
+    $prenom = "Annette";
+    $nom = "Bwemere";
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = ($note1 + $note2) / 2;
+    echo "Bonjour $prenom \n $nom Vous avez eu $moyenne"
 
 ?>
 
-// le script se termine ici, sans la balise de fermeture PHP
-
-
-<p>Ceci sera ignoré par PHP Et affiché au navigateur</p>
-<?php echo 'Alors que ceci sera analysé par PHP'; ?>
-<p>Ceci sera aussi ignore par PHP et affiché au navigateur.</p>
-
-<?php if ($expression == true): ?>
-    ceci sera afficher si l'$expression est vraie.
-    
-<?php else: ?>
-    sinon, ceci sera affiché.
- <?php endif; ?>       
-
- Exemple #1 montrant que la balise fermente englobe la nouvelle ligne qui sapi_windows_cp_is_utf8
- 
- 
- <?php echo "Some text"; ?>
- No newline
-
- <?= "But newline now" ?>
-
-
- 
