b1cf6ad919fb2585ad73ce619a1b9adb8de1202f
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 15:51:30 +0200

    feat: arraystringInt

--- demo.php
+++ demo.php
@@ -14,13 +14,36 @@ $mon_identité = [
     "nationalité"=>"congolaise",
     
 ];
-echo $mon_identité["nom"];
-echo $mon_identité["prenom"];
-echo $mon_identité["postnom"];
-echo $mon_identité["Age"];
-echo $mon_identité["genre"];
-echo $mon_identité["nationalité"];
 // echo $mon_identité["nom"];
+// echo $mon_identité["prenom"];
+// echo $mon_identité["postnom"];
+// echo $mon_identité["Age"];
+// echo $mon_identité["genre"];
+// echo $mon_identité["nationalité"];
+// echo $mon_identité["nationalité"];
+var_dump($mon_identité["nationalité"]);
+// var_dump($mon_identité)
+
+// echo $mon_identité["nom"];
+
+$ecrassement = [
+    1 => "annette",
+    "1" => "bwemere",
+    1.5 => "salama",
+    true => "Bwemere"
+
+];
+// var_dump($ecrassement)
+
+$string_Int = [
+    "name" => "annette",
+    "lastname" => "foo",
+    20 => "age",
+    -100 => 20,
+];
+
+var_dump($string_Int[20]);
+var_dump($string_Int)
 
 
 ?>
