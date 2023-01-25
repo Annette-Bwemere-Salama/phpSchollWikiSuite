f48fabf59e2f1d59eb5563c0f0375b0e5962c918
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 09:59:17 +0200

    feat: commentaire

---
+++ commentaire.php
@@ -0,0 +1,6 @@
+<?php echo "Ceci est un test"; //Ceci est un commentaire sur une seule ligne, style c++
+/*Ceci est un commentaire sur pmlusiers lignes*/ 
+
+echo "Ceci est un autre test";
+echo "Et un test final"; #Ceci est un commentaire style shell sur une seul ligne
+?>
