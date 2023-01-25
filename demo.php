210d154979d1b3872470a1bf30ff89ae6867b2f1
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 15:36:32 +0200

    feat: tableaux

--- demo.php
+++ demo.php
@@ -4,7 +4,24 @@
     $note1 = 10;
     $note2 = 20;
     $moyenne = ($note1 + $note2) / 2;
-    echo "Bonjour $prenom \n $nom Vous avez eu $moyenne"
+    // echo "\nBonjour $prenom\n$nom Vous avez eu $moyenne"
+$mon_identité = [
+    "nom" => "Annette",
+    "prenom" => "Bwemere",
+    "postnom" => "Salama",
+    "Age" => 20,
+    "genre"=>"feminin",
+    "nationalité"=>"congolaise",
+    
+];
+echo $mon_identité["nom"];
+echo $mon_identité["prenom"];
+echo $mon_identité["postnom"];
+echo $mon_identité["Age"];
+echo $mon_identité["genre"];
+echo $mon_identité["nationalité"];
+// echo $mon_identité["nom"];
+
 
 ?>
 
