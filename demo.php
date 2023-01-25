d9c38b8737bd3ac994240f7f0b6c7675dfad082b
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 09:16:23 +0200

    feat: baliseCourtePHP

--- demo.php
+++ demo.php
@@ -1,6 +1,5 @@
-Salut La famille ça va ?
-<?php
+<?php echo 'Si vous voulez integrer du code PHP Dans le document XHTML ou XML, Utilisez ces balises'?>
 
-?>
+vous pouvez utiliser la balise courte pour  <?= 'écrire ce texte'?>
 
-comment ça va ?
+<!-- comment ça va ? -->
