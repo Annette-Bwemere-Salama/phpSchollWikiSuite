c14583f4d2236a9958a91e6a060ba58b74fc05be
Annette Bwemere <bwemereannette@gmail.com>
2023-01-25 17:26:23 +0200

    tableaux

--- demo.php
+++ demo.php
@@ -14,37 +14,89 @@ $mon_identité = [
     "nationalité"=>"congolaise",
     
 ];
-// echo $mon_identité["nom"];
-// echo $mon_identité["prenom"];
-// echo $mon_identité["postnom"];
-// echo $mon_identité["Age"];
-// echo $mon_identité["genre"];
-// echo $mon_identité["nationalité"];
-// echo $mon_identité["nationalité"];
-var_dump($mon_identité["nationalité"]);
-// var_dump($mon_identité)
 
-// echo $mon_identité["nom"];
 
-$ecrassement = [
-    1 => "annette",
-    "1" => "bwemere",
-    1.5 => "salama",
-    true => "Bwemere"
+// $ecrassement = [
+//     1 => "annette",
+//     "1" => "bwemere",
+//     1.5 => "salama",
+//     true => "Bwemere"
 
-];
+// ];
 // var_dump($ecrassement)
 
-$string_Int = [
-    "name" => "annette",
-    "lastname" => "foo",
-    20 => "age",
-    -100 => 20,
+// $string_Int = [
+//     "name" => "annette",
+//     "lastname" => "foo",
+//     20 => "age",
+//     -100 => 20,
+// ];
+
+// var_dump($string_Int[20]);
+// var_dump($string_Int)
+
+
+// $indexSansCles = (
+
+//     "Ecole",
+//     "Famille",
+//     "Travail",
+//     "Amie",
+//     "Connaisance"
+// );
+// var_dump($indexSansClesinde);
+
+// $array = array("foo", "bar", "hello", "world");
+// var_dump($array);
+
+// $array = array(
+//     1    => 'a',
+//     '1'  => 'b', // la valeur "a" sera écrasé par "b"
+//     1.5  => 'c', // la valeur "b" sera écrasé par "c"
+//     -1 => 'd',
+//     '01'  => 'e', // comme ceci n'est pas une chaîne de caractère entière elle N'écrasera PAS la clé pour 1
+//     '1.5' => 'f', // comme ceci n'est pas une chaîne de caractère entière elle N'écrasera PAS la clé pour 1
+//     true => 'g', // la valeur "c" sera écrasé par "g"
+//     false => 'h',
+//     '' => 'i',
+//     null => 'j', // la valeur "i" sera écrasé par "j"
+//     'k', // la valeur "k" est assigné la clé 2. Ceci est du au fait que la dernière clé entière la plus grande utilisé auparavant était 1
+//     2 => 'l', // la valeur "k" sera écrasé par "l"
+// );
+// var_dump($array);
+
+
+
+// $notes = [
+//     10,
+//     20,
+//     10,
+//     9,
+//     8,
+//     'eleve'
+// ];
+// echo $notes[1];
+// // echo $notes[5];
+
+// $eleve = ['nom'=>'Annette', 'prenom'=>'Bwemere','notes'=> [10, 20, 30, 40]];
+// $eleve['prenom'] = 'Jean';
+// echo $eleve['prenom'] . ' ' . $eleve['nom'];
+
+
+$classe = [
+    [
+        'nom' => 'Annete',
+        'Prenom' => 'Bwemere',
+        'notes' => [16, 12, 17],
+    ],
+    [
+        'nom' => 'Arick',
+        'Prenom' => 'Bulakali',
+        'notes' => [12, 10, 11]
+    ]
+
 ];
 
-var_dump($string_Int[20]);
-var_dump($string_Int)
-
-
+echo $classe[1]['notes'][2]
 ?>
 
