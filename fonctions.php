98213f980377bc603c81ecfbdaf4eb114c5d3a36
Annette Bwemere <bwemereannette@gmail.com>
2023-01-31 09:41:31 +0200

    feat: renverseTble

--- fonctions.php
+++ fonctions.php
@@ -2,8 +2,10 @@
 $notes = [10,20,13];
 // $notes[] = 16;
 array_push($notes, 16, 17, 18);
+$notesReverse  = array_reverse($notes);
 $sum = array_sum($notes);
 $count = count($notes);
-print_r($notes)
+print_r($notes);
+print_r($notesReverse);
 // echo "Vous avez" . round($sum / $count, 2) . "de moyenne";
 ?>
