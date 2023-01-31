e46fbb15325b81dd6f6035c87fc9cb45ede19ad0
Annette Bwemere <bwemereannette@gmail.com>
2023-01-31 12:32:21 +0200

    feat:Palidrom

--- fonctions.php
+++ fonctions.php
@@ -1,11 +1,27 @@
 <?php
-$notes = [10,20,13];
+// $notes = [10,20,13];
+// $notes2 = $notes;
+// $notes2[] = 10;
+// var_dump($notes, $notes2);
 // $notes[] = 16;
-array_push($notes, 16, 17, 18);
-$notesReverse  = sort($notes);
-$sum = array_sum($notes);
-$count = count($notes);
-print_r($notes);
-print_r($notesReverse);
+// array_push($notes, 16, 17, 18);
+// $notesReverse  = sort($notes);
+// $sum = array_sum($notes);
+// $count = count($notes);
+// print_r($notes);
+// print_r($notesReverse);
 // echo "Vous avez" . round($sum / $count, 2) . "de moyenne";
+
+while(true){
+    $mot = readline('Entrez votre mot:');
+    if($mot === ""){
+        exit("Fin du programme");
+    }
+    $renverse = strrev($mot);
+    if(strtolower($mot) === $renverse){
+        echo "Ce mot est un plalindrome";
+    }else{
+        echo "c'est mot ne pas palidrome";
+    }
+}
 ?>
