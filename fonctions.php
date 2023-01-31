197518583025d5d5854e1279c57625c3877cf3b4
Annette Bwemere <bwemereannette@gmail.com>
2023-01-31 09:45:07 +0200

    feat: sort

--- fonctions.php
+++ fonctions.php
@@ -2,7 +2,7 @@
 $notes = [10,20,13];
 // $notes[] = 16;
 array_push($notes, 16, 17, 18);
-$notesReverse  = array_reverse($notes);
+$notesReverse  = sort($notes);
 $sum = array_sum($notes);
 $count = count($notes);
 print_r($notes);
