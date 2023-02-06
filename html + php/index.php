8e484340702ceebcc3325a9cc94da06048fb2024
Annette Bwemere <bwemereannette@gmail.com>
2023-02-06 12:31:48 +0200

    feat: affichageTitre

--- html + php/index.php
+++ html + php/index.php
@@ -1,4 +1,7 @@
-<?php require "header.php"; ?>
+<?php 
+$title = "page d'acceuil";
+require "header.php";
+?>
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -61,11 +64,11 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Cover</h3>
+      <h3 class="float-md-start mb-0">SOCONE.SARL</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
-        <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="index.php">Accueil</a>
+        <!-- <a class="nav-link fw-bold py-1 px-0" href="#">Features</a> -->
+        <a class="nav-link fw-bold py-1 px-0" href="Contact.php">Contact</a>
       </nav>
     </div>
   </header>
@@ -83,6 +86,8 @@
 
 
   <?php  require "footer.php"?>  
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
   </body>
 </html>
 
